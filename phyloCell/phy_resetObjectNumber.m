5485946c33d81b2269f2e72e887be73eb9fd0112
gcharvin <gilles.charvin@gmail.com>
2014-02-20 14:01:20 +0100

    started gap closing function for cell traj

---
+++ phyloCell/phy_resetObjectNumber.m
@@ -0,0 +1,18 @@
+function phy_resetObjectNumber(objectname) % rest the numbers of object // this discards the current mapping
+global segmentation timeLapse;
+
+segmentation.(['t' objectname])=phy_Tobject;
+object=segmentation.(objectname);
+segmentation.([objectname 'Mapped'])=zeros(1,timeLapse.numberOfFrames);
+
+
+for i=1:length(object(:,1))
+    cc=1;
+    for j=1:length(object(i,:))
+    if object(i,j).n~=0
+        object(i,j).n=cc;
+        cc=cc+1;
+    end
+    end
+end
+
